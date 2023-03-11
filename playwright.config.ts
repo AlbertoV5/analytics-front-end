fea05fb1e5c42921ef51c7ce53de654335d7f604
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2023-03-10 20:25:32 -0600

    improved github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,15 +31,20 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  webServer: {
+    command: 'npm run preview',
+    url: 'http://localhost:3000',
+    timeout: 240 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.HAMX_BASE_URL,
+    baseURL: 'http://localhost:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
-
   /* Configure projects for major browsers */
   projects: [
     {
