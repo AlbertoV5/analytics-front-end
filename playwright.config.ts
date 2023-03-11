4a76b9bee31ea72ee04e49715d6804b33b42f615
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2023-03-10 19:50:20 -0600

    added server to playwroght config and test

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,12 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  webServer: {
+    command: 'npm run preview',
+    url: 'http://localhost:3000/',
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
