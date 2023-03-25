0608dbce4e49b4daa957fb68bce2712f0088b409
AlbertoV5 <58243333+AlbertoV5@users.noreply.github.com>
2023-03-24 19:30:07 -0600

    fixed page titles

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -1,6 +1,18 @@
 import { test, expect } from '@playwright/test';
 
-test('meta is correct', async ({ page }) => {
-  await page.goto("/");
-  await expect(page).toHaveTitle('Health Analytics MX');
+
+test.describe('Verify Page Titles. TODO: Support.', async () => {
+    const links: {title: string, href: string}[] = [
+        {title: "Calculators", href: "calculators"},
+        {title: "Metrics", href: "metrics"},
+        {title: "Database", href: "database"},
+        {title: "", href: "support"}, // TODO: FIX!
+        {title: "Login", href: "login"},
+    ];
+    for (const item of links) {
+        test(`testing with ${item.title}`, async ({page}) => {
+            await page.goto(`/${item.href}`);
+            await expect(page).toHaveTitle(item.title);
+        });
+    }
 });
